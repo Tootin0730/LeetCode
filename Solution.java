d584a0965755e4bfd8756d430960e83c0fea25a3
Tootin0730 <yisan1004@gmail.com>
2024-09-07 12:51:56 -0400

    Remove Duplicates from sorted Array

--- Solution.java
+++ Solution.java
@@ -1,11 +1,14 @@
 public class Solution {
-    public int removeElement(int[] nums, int val) {
-        int k = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != val) {
-                nums[k++] = nums[i];
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0) return 0;  // Edge case for empty array
+        
+        int k = 1;  // k will track the index of the next unique element
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {  // Compare with the previous element
+                nums[k] = nums[i];  // Place the unique element in position k
+                k++;  // Increment k to move to the next position
             }
         }
-        return k;
+        return k;  // Return the number of unique elements
     }
 }
