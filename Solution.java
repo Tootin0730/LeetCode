20d75f614f0f41d18155d8ecfced1785ba199277
Tootin0730 <yisan1004@gmail.com>
2024-09-10 12:42:36 -0400

    Majority Element

--- Solution.java
+++ Solution.java
@@ -1,17 +1,18 @@
 public class Solution {
-    public int removeDuplicates(int[] nums) {
-        int n = nums.length;
-        if (n <= 2) return n; // If the array has 2 or fewer elements, no need to process.
+    public int majorityElement(int[] nums) {
+        int candidate = nums[0];
+        int count = 1;
 
-        int j = 2; // Start updating from the 3rd element
-        for (int i = 2; i < n; i++) {
-            // If the current element is not equal to the element two places before it, then it is safe to include it.
-            if (nums[i] != nums[j - 2]) {
-                nums[j] = nums[i];
-                j++;
+        for (int i = 1; i < nums.length; i++) {
+            if (count == 0) {
+                candidate = nums[i];
+                count = 1;
+            } else if (nums[i] == candidate) {
+                count++;
+            } else {
+                count--;
             }
         }
-        
-        return j; // 'j' is the new length of the modified array
+        return candidate;
     }
 }
