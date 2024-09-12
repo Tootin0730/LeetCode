7e27bb0e1f62a9e48842c2a5f19bde6ec2673226
Tootin0730 <yisan1004@gmail.com>
2024-09-12 11:26:05 -0400

    Best time to buy and sell stock

--- Solution.java
+++ Solution.java
@@ -1,26 +1,23 @@
 public class Solution {
-    public void rotate(int[] nums, int k) {
-        int n = nums.length;
-        k = k % n; // Handle cases where k > n
+    public int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
 
-        // Step 1: Reverse the entire array
-        reverse(nums, 0, n - 1);
-
-        // Step 2: Reverse the first k elements
-        reverse(nums, 0, k - 1);
-
-        // Step 3: Reverse the remaining elements
-        reverse(nums, k, n - 1);
-    }
-
-    // Helper method to reverse a portion of the array
-    private void reverse(int[] nums, int start, int end) {
-        while (start < end) {
-            int temp = nums[start];
-            nums[start] = nums[end];
-            nums[end] = temp;
-            start++;
-            end--;
+        for (int price : prices) {
+            // Update the minimum price if the current price is lower
+            if (price < minPrice) {
+                minPrice = price;
+            }
+            // Calculate profit if sold today and update max profit
+            else {
+                int profit = price - minPrice;
+                if (profit > maxProfit) {
+                    maxProfit = profit;
+                }
+            }
         }
+
+        return maxProfit;
     }
 }
+
