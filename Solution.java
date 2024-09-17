64e642e2de29243133cc3ab1dec85eeb3890b3b4
Tootin0730 <yisan1004@gmail.com>
2024-09-17 12:03:13 -0400

    Jump Game

--- Solution.java
+++ Solution.java
@@ -1,15 +1,18 @@
 public class Solution {
-    public int maxProfit(int[] prices) {
-        int maxProfit = 0;
-        
-        for (int i = 1; i < prices.length; i++) {
-            // Add the profit if the current price is higher than the previous price
-            if (prices[i] > prices[i - 1]) {
-                maxProfit += prices[i] - prices[i - 1];
+    public boolean canJump(int[] nums) {
+        int farthest = 0;  // Tracks the farthest index we can reach
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i > farthest) {
+                return false;  // If the current index is beyond the farthest point we can reach, return false
+            }
+            farthest = Math.max(farthest, i + nums[i]);  // Update the farthest point we can reach
+            if (farthest >= nums.length - 1) {
+                return true;  // If the farthest point reaches or exceeds the last index, return true
             }
         }
-        
-        return maxProfit;
+
+        return false;  // Return false if we finish the loop without reaching the last index
     }
 }
 
