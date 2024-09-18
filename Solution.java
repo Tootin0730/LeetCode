593d4436ee164c60e30e222ab04066100d2cabe1
Tootin0730 <yisan1004@gmail.com>
2024-09-18 10:12:09 -0400

    Jump Game 2

--- Solution.java
+++ Solution.java
@@ -1,18 +1,25 @@
 public class Solution {
-    public boolean canJump(int[] nums) {
-        int farthest = 0;  // Tracks the farthest index we can reach
+    public int jump(int[] nums) {
+        int jumps = 0;
+        int currentEnd = 0;  // End of the range we can reach with current jumps
+        int farthest = 0;  // Farthest we can reach from current index
 
-        for (int i = 0; i < nums.length; i++) {
-            if (i > farthest) {
-                return false;  // If the current index is beyond the farthest point we can reach, return false
-            }
-            farthest = Math.max(farthest, i + nums[i]);  // Update the farthest point we can reach
-            if (farthest >= nums.length - 1) {
-                return true;  // If the farthest point reaches or exceeds the last index, return true
+        for (int i = 0; i < nums.length - 1; i++) {
+            // Update the farthest point we can reach
+            farthest = Math.max(farthest, i + nums[i]);
+
+            // If we have reached the end of the range for the current jump, we make another jump
+            if (i == currentEnd) {
+                jumps++;
+                currentEnd = farthest;  // Move to the next range
+
+                // If the farthest point reaches or exceeds the last index, return jumps
+                if (currentEnd >= nums.length - 1) {
+                    break;
+                }
             }
         }
 
-        return false;  // Return false if we finish the loop without reaching the last index
+        return jumps;
     }
 }
-
