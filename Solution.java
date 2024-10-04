799dc007276acd8f8c041c8263315eee1c74df68
Tootin0730 <yisan1004@gmail.com>
2024-10-04 14:32:31 -0400

    Reverse Words in a String

--- Solution.java
+++ Solution.java
@@ -1,34 +1,22 @@
 public class Solution {
+    public String reverseWords(String s) {
+        String[] words = s.trim().split("\\s+");
+        StringBuilder reversed = new StringBuilder();
 
-    public String longestCommonPrefix(String[] strs) {
-        if (strs == null || strs.length == 0) {
-            return "";
-        }
-        
-        // Find the shortest string in the array
-        String prefix = strs[0];
-        
-        for (int i = 1; i < strs.length; i++) {
-            // Compare prefix with the current string
-            while (strs[i].indexOf(prefix) != 0) {
-                // Gradually reduce the length of the prefix
-                prefix = prefix.substring(0, prefix.length() - 1);
-                
-                // If the prefix becomes empty, return ""
-                if (prefix.isEmpty()) {
-                    return "";
-                }
+        for (int i = words.length - 1; i >= 0; i--) {
+            reversed.append(words[i]);
+            if (i != 0) {
+                reversed.append(" ");
             }
         }
-        
-        return prefix;
+        return reversed.toString();
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        // Test cases
-        System.out.println(solution.longestCommonPrefix(new String[]{"flower","flow","flight"}));  // Output: "fl"
-        System.out.println(solution.longestCommonPrefix(new String[]{"dog","racecar","car"}));     // Output: ""
+        System.out.println(solution.reverseWords("the sky is blue"));         // Output: "blue is sky the"
+        System.out.println(solution.reverseWords("  hello world  "));         // Output: "world hello"
+        System.out.println(solution.reverseWords("a good   example"));        // Output: "example good a"
     }
 }
