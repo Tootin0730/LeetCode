24cc4077767cc84fecd06ce53545142f46a24a46
Tootin0730 <yisan1004@gmail.com>
2024-10-14 02:03:56 -0400

    Two Sum 2 - Input Array Is sorted

--- Solution.java
+++ Solution.java
@@ -1,19 +1,19 @@
 class Solution {
-    public boolean isSubsequence(String s, String t) {
+    public int[] twoSum(int[] numbers, int target) {
+        int left = 0, right = numbers.length - 1;
         
-        if (s.length() == 0) return true;       // Edge case: If s is empty, it's always a subsequence
-        int sIndex = 0, tIndex = 0;     // Pointers for s and t
-        
-        while (tIndex < t.length()) {       // Iterate through t
-            if (t.charAt(tIndex) == s.charAt(sIndex)) {     // If characters match, move the pointer for s
-                sIndex++;
-                if (sIndex == s.length()) {     // If we've traversed the entire string s, it's a subsequence
-                    return true;
-                }
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+            
+            if (sum == target) {
+                return new int[] {left + 1, right + 1};
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
             }
-            tIndex++;       // Move the pointer for t
         }
         
-        return false;       // If we exit the loop and haven't matched all characters in s, it's not a subsequence
+        return new int[] {};
     }
 }
