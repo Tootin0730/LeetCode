d6b1ccc120c9de4d6c95de22cbdb4e6304b23e36
Tootin0730 <yisan1004@gmail.com>
2024-10-21 16:21:35 -0400

    Substring with Concatentation of All Words

--- Solution.java
+++ Solution.java
@@ -1,21 +1,38 @@
-import java.util.HashSet;
-
 class Solution {
-    public int lengthOfLongestSubstring(String s) {
-        int left = 0, right = 0, maxLength = 0;
-        HashSet<Character> charSet = new HashSet<>();
-        
-        while (right < s.length()) {
-            if (!charSet.contains(s.charAt(right))) {
-                charSet.add(s.charAt(right));
-                maxLength = Math.max(maxLength, right - left + 1);
-                right++;
-            } else {
-                charSet.remove(s.charAt(left));
-                left++;
+    public List<Integer> findSubstring(String s, String[] words) {
+        Map<String, Integer> cnt = new HashMap<>();
+        for (String w : words) {
+            cnt.merge(w, 1, Integer::sum);
+        }
+        int m = s.length(), n = words.length;
+        int k = words[0].length();
+        List<Integer> ans = new ArrayList<>();
+        for (int i = 0; i < k; ++i) {
+            Map<String, Integer> cnt1 = new HashMap<>();
+            int l = i, r = i;
+            int t = 0;
+            while (r + k <= m) {
+                String w = s.substring(r, r + k);
+                r += k;
+                if (!cnt.containsKey(w)) {
+                    cnt1.clear();
+                    l = r;
+                    t = 0;
+                    continue;
+                }
+                cnt1.merge(w, 1, Integer::sum);
+                ++t;
+                while (cnt1.get(w) > cnt.get(w)) {
+                    String remove = s.substring(l, l + k);
+                    l += k;
+                    cnt1.merge(remove, -1, Integer::sum);
+                    --t;
+                }
+                if (t == n) {
+                    ans.add(l);
+                }
             }
         }
-        
-        return maxLength;
+        return ans;
     }
 }
