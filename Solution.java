12129ae6cd98c845c0393e9a93954a7962e95193
Tootin0730 <yisan1004@gmail.com>
2024-10-25 12:36:37 -0400

    Rotate Image

--- Solution.java
+++ Solution.java
@@ -2,45 +2,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Solution {
-    public List<Integer> spiralOrder(int[][] matrix) {
+    public void rotate(int[][] matrix) {
 
-        List<Integer> result = new ArrayList<>();
-
-        if (matrix == null || matrix.length == 0) {
-            return result;
-        }
+      List<Integer> result = new ArrayList<>();
 
         int top = 0;
         int bottom = matrix.length - 1;
         int left = 0;
         int right = matrix[0].length - 1;
 
-        while (top <= bottom && left <= right) {
-            
-            for (int i = left; i <= right; i++) {
-                result.add(matrix[top][i]);
+        while(left <= right){
+            for (int i = bottom; i >= 0; i--){
+                result.add(matrix[i][left]);
             }
-            top++;
-
-            for (int i = top; i <= bottom; i++) {
-                result.add(matrix[i][right]);
-            }
-            right--;
-
-            if (top <= bottom) {
-                for (int i = right; i >= left; i--) {
-                    result.add(matrix[bottom][i]);
-                }
-                bottom--;
-            }
-
-            if (left <= right) {
-                for (int i = bottom; i >= top; i--) {
-                    result.add(matrix[i][left]);
-                }
-                left++;
+            left++;
+        }
+        
+        int index = 0;
+        for(int vertic = 0; vertic <= bottom; vertic++){       
+            for(int horizon = 0; horizon <= right; horizon++){
+                matrix[vertic][horizon] = result.get(index);
+                index++;
             }
         }
-        return result;
     }
 }
