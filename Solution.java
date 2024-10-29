762aadc5a0650edbc6983354962284ad888f9127
Tootin0730 <yisan1004@gmail.com>
2024-10-29 12:42:05 -0400

    Game of Life

--- Solution.java
+++ Solution.java
@@ -1,37 +1,37 @@
-import java.util.ArrayList;
-
 class Solution {
-    public void setZeroes(int[][] matrix) {
-        int top = 0;
-        int bottom = matrix.length - 1;
-        int left = 0;
-        int right = matrix[0].length - 1;
-
-        ArrayList<Integer> coordRow = new ArrayList<Integer>();
-        ArrayList<Integer> coordColumn = new ArrayList<Integer>();
-
-        for(int i = top; i <= bottom; i++){
-            for(var j = left; j <= right; j++){
-                if(matrix[i][j] == 0){
-                    coordRow.add(i);
-                    coordColumn.add(j);
+    public void gameOfLife(int[][] board) {
+        int m = board.length;
+        int n = board[0].length;
+        
+        int[][] directions = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int liveNeighbors = 0;
+                
+                for (int[] direction : directions) {
+                    int ni = i + direction[0];
+                    int nj = j + direction[1];
+                    if (ni >= 0 && ni < m && nj >= 0 && nj < n && Math.abs(board[ni][nj]) == 1) {
+                        liveNeighbors++;
+                    }
+                }
+                
+                if (board[i][j] == 1 && (liveNeighbors < 2 || liveNeighbors > 3)) {
+                    board[i][j] = -1;
+                } else if (board[i][j] == 0 && liveNeighbors == 3) {
+                    board[i][j] = 2;
                 }
             }
         }
         
-        int colSize = coordColumn.size(), colIndex = 0;
-        for(int a = 0; a < colSize; a++){
-            for(int i = top; i <= bottom; i++){
-                colIndex = coordColumn.get(a);
-                matrix[i][colIndex] = 0;
-            }
-        }
-
-        int rowSize = coordRow.size(), rowIndex = 0;
-        for(int b = 0; b < rowSize; b++){
-            for(int i = left; i <= right; i++){
-                rowIndex = coordRow.get(b);
-                matrix[rowIndex][i] = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == -1) {
+                    board[i][j] = 0;
+                } else if (board[i][j] == 2) {
+                    board[i][j] = 1;
+                }
             }
         }
     }
