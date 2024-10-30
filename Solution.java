8e0d0570da0fbb4df25cf68d26fefaba7b48bc5f
Tootin0730 <yisan1004@gmail.com>
2024-10-30 11:31:01 -0400

    Ransom Note

--- Solution.java
+++ Solution.java
@@ -1,38 +1,18 @@
 class Solution {
-    public void gameOfLife(int[][] board) {
-        int m = board.length;
-        int n = board[0].length;
-        
-        int[][] directions = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
-        
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                int liveNeighbors = 0;
-                
-                for (int[] direction : directions) {
-                    int ni = i + direction[0];
-                    int nj = j + direction[1];
-                    if (ni >= 0 && ni < m && nj >= 0 && nj < n && Math.abs(board[ni][nj]) == 1) {
-                        liveNeighbors++;
-                    }
-                }
-                
-                if (board[i][j] == 1 && (liveNeighbors < 2 || liveNeighbors > 3)) {
-                    board[i][j] = -1;
-                } else if (board[i][j] == 0 && liveNeighbors == 3) {
-                    board[i][j] = 2;
-                }
-            }
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] charCount = new int[26];
+
+        for (char c : magazine.toCharArray()) {
+            charCount[c - 'a']++;
         }
-        
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (board[i][j] == -1) {
-                    board[i][j] = 0;
-                } else if (board[i][j] == 2) {
-                    board[i][j] = 1;
-                }
+
+        for (char c : ransomNote.toCharArray()) {
+            if (charCount[c - 'a'] == 0) {
+                return false; 
             }
+            charCount[c - 'a']--; 
         }
+
+        return true; 
     }
 }
