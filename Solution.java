d34f4d8f911e3e9f9a33d10f2e13827a321ab4d1
Tootin0730 <yisan1004@gmail.com>
2024-12-04 17:30:28 -0500

    Copy List With Random Pointer

--- Solution.java
+++ Solution.java
@@ -1,25 +1,35 @@
 class Solution {
-    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode dummy = new ListNode(-1);
-        ListNode current = dummy;
-
-        while (list1 != null && list2 != null) {
-            if (list1.val <= list2.val) {
-                current.next = list1;
-                list1 = list1.next;
-            } else {
-                current.next = list2;
-                list2 = list2.next;
+    public Node copyRandomList(Node head) {
+        if (head == null) return null;
+        
+        Node current = head;
+        while (current != null) {
+            Node copy = new Node(current.val);
+            copy.next = current.next;
+            current.next = copy;
+            current = copy.next;
+        }
+        
+        current = head;
+        while (current != null) {
+            if (current.random != null) {
+                current.next.random = current.random.next;
+            }
+            current = current.next.next;
+        }
+        
+        current = head;
+        Node copiedHead = head.next;
+        Node copiedCurrent = copiedHead;
+        while (current != null) {
+            current.next = current.next.next;
+            if (copiedCurrent.next != null) {
+                copiedCurrent.next = copiedCurrent.next.next;
+                copiedCurrent = copiedCurrent.next;
             }
             current = current.next;
         }
-
-        if (list1 != null) {
-            current.next = list1;
-        } else {
-            current.next = list2;
-        }
-
-        return dummy.next;
+        
+        return copiedHead;
     }
 }
